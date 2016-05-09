4df0d9b06bcc3b709f57bb139ca08c4e43d8c70f
caoyongzheng <15267056106@139.com>
2016-05-09 17:43:15 +0800

    add webSecurity

--- src/main/java/com/itbegin/outprojs/microcard/config/security/SecurityConfig.java
+++ src/main/java/com/itbegin/outprojs/microcard/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,11 +23,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()                                                              
-				.antMatchers("/get/**", "/edit/**","/add/**").permitAll()                  
-				.antMatchers("/test").hasAnyRole("USER","ADMIN")                                 
+				.antMatchers("/get/**", "/test","/add/**").permitAll()                  
+				.antMatchers("/edit/**").hasAnyRole("USER","ADMIN")                                 
 				.anyRequest().authenticated()
 			.and()
 			.formLogin();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	  web.ignoring().antMatchers("/js/**","/css/**");
 	}
 
 }
